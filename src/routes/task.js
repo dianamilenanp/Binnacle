41e94fb9049c153544c5a6b1f97f3b5f11b51276
camorabo <camorabo@unal.edu.co>
2022-10-19 09:48:03 -0500

    Commit 4 - Creación endpoint nueva actividad
    Se incluye el post para la nueva actividad.

--- src/routes/task.js
+++ src/routes/task.js
@@ -1,3 +1,12 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const taskSchema = require("../models/task");
+
+ //Nueva Actividad
+ router.post("/actividad", (req, res) => {
+    const task = taskSchema(req.body);
+    task
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
